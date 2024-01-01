98b2abfb0e72fc21ceb90d4a340bff6fe96c424d
mutsusuke <mtsk5115@gmail.com>
2024-01-01 13:23:00 +0900

    typescriptに変換 & added auth

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import "@aws-amplify/ui-react/styles.css";
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
+
+function App() {
+
+  const signOut=()=>{
+
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <View className="App">
+      <Card>
+        <Image src={logo} className="App-logo" alt="logo" />
+        <Heading level={1}>We now have Auth!</Heading>
+      </Card>
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
+    </div>
+  );
+}
+
+export default withAuthenticator(App);
+
